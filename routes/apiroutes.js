59d7f886563972796b69c5f316a9ce8b7bafc5fa
James Rowe <jamesrowe@outlook.com>
2020-07-30 18:46:20 -0400

    got delete functionality to work

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -19,16 +19,26 @@ router.get("/api/notes", async (req, res) => {
 
 router.post("/api/notes", async (req, res) => {
   //add an id = current length of the array
-  req.body.id = parseInt(notes[notes.length - 1].id) + 1;
-  console.log(req.body);
+  if (notes.length !== 0) {
+    req.body.id = parseInt(notes[0].id) + 1;
+  } else {
+    req.body.id = 0;
+  }
+  console.log(req.body.id);
   //assign id as index in array
-  notes.push(req.body);
+  notes.unshift(req.body);
   res.json(notes);
   fs.writeFileSync("./db/db.json", JSON.stringify(notes));
 });
 
-router.delete("/api/notes", async (req, res) => {
+router.delete("/api/notes/:id", async (req, res) => {
   //   res.json(notes);
+  let id = parseInt(req.params.id);
+
+  notes = notes.filter((note) => note.id !== id);
+  console.log(notes);
+  res.json(notes);
+  fs.writeFileSync("./db/db.json", JSON.stringify(notes));
 });
 
 module.exports = router;
