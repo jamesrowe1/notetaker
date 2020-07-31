46030d385a373a988e8b492755a4d897176c35f6
James Rowe <jamesrowe@outlook.com>
2020-07-31 14:46:43 -0400

    added comments

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -5,6 +5,7 @@ const path = require("path");
 const fs = require("fs");
 let notes = "";
 
+//read the db.json file, parse it and put it into notes
 fs.readFile("./db/db.json", "utf8", (err, data) => {
   if (err) {
     console.log(err);
@@ -13,24 +14,26 @@ fs.readFile("./db/db.json", "utf8", (err, data) => {
   }
 });
 
+//this is the api holding all the notes
 router.get("/api/notes", async (req, res) => {
   res.json(notes);
 });
 
+//when the note is made and a post request sent, send the req.body info to both notes and the db.json file
 router.post("/api/notes", async (req, res) => {
-  //add an id = current length of the array
+  //add an id = 0 if empty array, otherwise id of last item + 1
   if (notes.length !== 0) {
     req.body.id = parseInt(notes[0].id) + 1;
   } else {
     req.body.id = 0;
   }
-  console.log(req.body.id);
   //assign id as index in array
   notes.unshift(req.body);
   res.json(notes);
   fs.writeFileSync("./db/db.json", JSON.stringify(notes));
 });
 
+//when a delete request is sent, delete the identified note from notes and rewrite db.json without it
 router.delete("/api/notes/:id", async (req, res) => {
   //   res.json(notes);
   let id = parseInt(req.params.id);
@@ -41,4 +44,5 @@ router.delete("/api/notes/:id", async (req, res) => {
   fs.writeFileSync("./db/db.json", JSON.stringify(notes));
 });
 
+//export
 module.exports = router;
--- server.js
+++ server.js
@@ -9,15 +9,18 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 9000;
 
+//set up the parser to make stuff readable
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
+//use the apiroutes and htmlroutes files
 app.use(apiroutes);
 app.use(htmlroutes);
 
+//start the server
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
